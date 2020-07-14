384c8356cbda04b5d881d5d649ed482cd040c646
nehruperumalla1 <nehruperumalla@msitprogram.net>
2020-07-14 12:42:09 +0530

    feat: Created routes for API and Added Skeleton of  API

---
+++ routes/parking_api.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const router = express.Router();
+
+//Park the Car
+router.post('/', (req, res) => {})
+
+//Unpark the Car
+router.delete('/', (req,res) => {})
+
+//Info of Car/Slot
+router.get('/', (req, res) => {})
