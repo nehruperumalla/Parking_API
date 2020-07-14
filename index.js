39b22d7c959dd0c7f08584e940e7ee926451e135
nehruperumalla1 <nehruperumalla@msitprogram.net>
2020-07-14 21:10:55 +0530

    feat: Added rate-limit to API endpoints

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import parkingRoutes from './routes/parking_api.js';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
 const PORT = 5000;
+
+const limiter = rateLimit({
+    windowMs: 10 * 1000,
+    max: 10,
+    message: 'Too many request from this IP, please try again later :)'
+});
+
+app.use(limiter);
 
 app.use(bodyParser.json());
 
